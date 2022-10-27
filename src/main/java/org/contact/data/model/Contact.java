722e0a86a047db698843cc31e16f708aedb6b4bb
Ola Lekan <lekan.sofuyi01@gmail.com>
2022-10-27 19:25:29 +0100

    save feature to contatc

--- src/main/java/org/contact/data/model/Contact.java
+++ src/main/java/org/contact/data/model/Contact.java
@@ -1,18 +1,24 @@
 package org.contact.data.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
 import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
 
-
 @Data
+@AllArgsConstructor
+@RequiredArgsConstructor
 @Builder
-public class Contact {
+@Document
+public class ContactModel {
     @Id
-    private int id;
+    private String id;
     private String firstName;
     private String lastName;
     private Set<String> phoneNumber = new HashSet<>();
